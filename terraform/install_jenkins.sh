79f474914a60a92cda459d3c468045bb39f9ebfe
Taidgh Robinson <taidgh.robinson@maine.edu>
2024-03-17 13:45:12 -0700

    Fix jenkins key

--- terraform/install_jenkins.sh
+++ terraform/install_jenkins.sh
@@ -23,7 +23,7 @@ sudo chkconfig docker on
 
 echo "Install Jenkins"
 sudo wget -O /etc/yum.repos.d/jenkins.repo https://pkg.jenkins.io/redhat-stable/jenkins.repo
-sudo rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io.key
+sudo rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io-2023.key
 sudo yum -y install jenkins
 sudo usermod -a -G docker jenkins
 sudo chkconfig jenkins on
